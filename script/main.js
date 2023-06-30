a3c1bf3b2f0b690b7f73b03693ded01be76912c9
Leandro Toucedo <turambar013@gmail.com>
2023-06-30 03:22:43 -0300

    Repetición de la función agregarDisco

--- script/main.js
+++ script/main.js
@@ -6,13 +6,23 @@ let usuario = {
 }
 
 let ingresarUsuario = () => {
-    let ingresarNombre = prompt ('Ingrese su nombre:');
-    usuario.nombre.push (ingresarNombre);
-    let ingresarPais = prompt ('Ingrese su país:');
-    usuario.pais.push (ingresarPais);
-    let ingresarMail = prompt ('Ingrese su mail:');
-    usuario.mail.push (ingresarMail);
-}
+    let ingresarNombre, ingresarPais, ingresarMail;
+    let camposVacios = true;
+    while (camposVacios){
+        ingresarNombre = prompt ('Ingrese su nombre:');
+        ingresarPais = prompt ('Ingrese su país:');
+        ingresarMail = prompt ('Ingrese su mail:');
+        if (ingresarNombre.trim () !== '' && ingresarPais.trim () !== '' && ingresarMail.trim () !== ''){
+            camposVacios = false;
+        } else {
+            console.log ('Debe ingresar algún valor en todos los campos')
+        }
+    }
+        usuario.nombre.push (ingresarNombre);
+        usuario.pais.push (ingresarPais);
+        usuario.mail.push (ingresarMail);
+
+};
 ingresarUsuario ();
 
 
@@ -33,9 +43,11 @@ function mostrarGeneros (){
 }
 
 function agregarDisco(){
-    mostrarGeneros();
+    let continuar = true;
+    do {
+        mostrarGeneros();
 
-    const opcion = parseInt (prompt ('Elija un género para agregar un disco(Ingrese el número correspondiente)'));
+        const opcion = parseInt (prompt ('Elija un género para agregar un disco(Ingrese el número correspondiente)'));
         if (opcion >= 1 && opcion <= generos.length) {
             const generoSeleccionado = generos [opcion-1];
             const artista = prompt ('Nombre del artista')
@@ -44,9 +56,13 @@ function agregarDisco(){
             usuario.discosGuardados.push ({nombre: disco, artista: artista});
 
             console.log ('El disco ' +disco + ' de ' +artista+ ' se agregó al género ' +generoSeleccionado.nombre);
+
+            const respuesta = prompt ('¿Desea agregar otro disco? s/SI (Cualquier otra tecla para finalizar)').toLowerCase();
+            continuar = respuesta === 's';
         } else {
             console.log ('Opcón inválida, elija un número de la lista de géneros');
         }
+    }while (continuar);
 }
 console.log (usuario);
 
